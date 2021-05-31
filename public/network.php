ba2e2a7916ddb337980f95172a1625fad8b05a61
pavamana manjithaya <18pavamanamanjithaya@gmail.com>
2021-05-31 08:08:44 +0530

    final

--- public/network.php
+++ public/network.php
@@ -1,28 +1,12 @@
 
 <?php
 try{
-	/whether ip is from share internet
-if (!empty($_SERVER['HTTP_CLIENT_IP']))   
-  {
-    $ip_address = $_SERVER['HTTP_CLIENT_IP'];
-  }
-//whether ip is from proxy
-elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
-  {
-    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
-  }
-//whether ip is from remote address
-else
-  {
-    $ip_address = $_SERVER['REMOTE_ADDR'];
-  }
-echo $ip_address;
 	echo"<table id='details'>";
 	echo"<tr>";
 	echo"<th colspan='2'>! Details !</th>";
 	echo"</tr>";
  $apikey='at_NXLDikD5UVSkWkTBt3ooGRuVrcJ7j';
- $url = 'https://geo.ipify.org/api/v1?apiKey='.$apikey.'&format=json&ipAddress='.$ip_address;
+ $url = 'https://geo.ipify.org/api/v1?apiKey='.$apikey.'&format=json&ipAddress=';
  $response = file_get_contents($url);
  $json_array=json_decode($response,true);
  function display_array_recursive($json_rec){
